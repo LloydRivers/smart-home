04b66117e40acd997210ac2a466c691239177fff
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-01-31 19:08:03 +0000

    di

--- src/base/Observable.ts
+++ src/base/Observable.ts
@@ -1,17 +1,25 @@
-import { IEvent, IObservable, IObserver } from "../interfaces";
+import { IEvent, IObservable, IObserver, ILogger } from "../interfaces";
 
 export abstract class Observable implements IObservable {
   private observers: Set<IObserver> = new Set();
+  private logger: ILogger;
+
+  constructor(logger: ILogger) {
+    this.logger = logger;
+  }
 
   subscribe(observer: IObserver): void {
     this.observers.add(observer);
+    this.logger.log("INFO", "Observer subscribed", { observer });
   }
 
   unsubscribe(observer: IObserver): void {
     this.observers.delete(observer);
+    this.logger.log("INFO", "Observer unsubscribed", { observer });
   }
 
   notify(event: IEvent): void {
+    this.logger.log("INFO", "Notifying observers", { event });
     this.observers.forEach((observer) => observer.update(event));
   }
 }
