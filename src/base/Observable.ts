148562e81411227c884c2f1b9ed7ee3b6773a585
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-01-31 20:33:21 +0000

    use the logger

--- src/base/Observable.ts
+++ src/base/Observable.ts
@@ -4,23 +4,23 @@ export abstract class Observable implements IObservable {
   private observers: Set<IObserver> = new Set();
   private logger: ILogger;
 
-  // An example of dependency injection
+  // Dependency Injection for logger
   constructor(logger: ILogger) {
     this.logger = logger;
   }
 
   subscribe(observer: IObserver): void {
     this.observers.add(observer);
-    this.logger.log("INFO", "Observer subscribed", { observer });
+    this.logger.info(`Observer subscribed`, { observer });
   }
 
   unsubscribe(observer: IObserver): void {
     this.observers.delete(observer);
-    this.logger.log("INFO", "Observer unsubscribed", { observer });
+    this.logger.info("Observer unsubscribed", { observer });
   }
 
   notify(event: IEvent): void {
-    this.logger.log("INFO", "Notifying observers", { event });
+    this.logger.info("Notifying observers", { event });
     this.observers.forEach((observer) => observer.update(event));
   }
 }
