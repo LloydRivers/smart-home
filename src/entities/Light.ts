4bafd35364ea85abdb889b82af5d140008098728
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-07 11:07:41 +0000

    add Observable + Notify (event driven)

--- src/entities/Light.ts
+++ src/entities/Light.ts
@@ -1,8 +1,15 @@
-export class Light {
+import { Observable } from "../base/Observable";
+
+export class Light extends Observable {
   private state: boolean = false;
 
   setOn(state: boolean): void {
     this.state = state;
+    this.notify({
+      type: "LIGHT_UPDATE",
+      timestamp: new Date(),
+      payload: { action: "on", state },
+    });
   }
 
   getOn(): boolean {
