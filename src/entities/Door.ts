29ac1b05e29633844025d1325ce8886e1ffcb73c
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-07 11:07:26 +0000

    add Observable + Notify to Door (event driven)

--- src/entities/Door.ts
+++ src/entities/Door.ts
@@ -1,8 +1,15 @@
-export class Door {
+import { Observable } from "../base/Observable";
+
+export class Door extends Observable {
   private locked: boolean = true;
 
   setLocked(state: boolean): void {
     this.locked = state;
+    this.notify({
+      type: "DOOR_UPDATE",
+      timestamp: new Date(),
+      payload: { action: "lock", state },
+    });
   }
 
   getLocked(): boolean {
