04b698000bc40e3e6fac77b588b83b49e4daf020
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-07 15:25:29 +0000

    refactor FireSafety class

--- src/features/FireSafety.ts
+++ src/features/FireSafety.ts
@@ -1,45 +1,21 @@
-import { IEvent, IObserver } from "../interfaces/index";
-
-import { Sprinkler } from "../entities/Sprinkler";
-import { MotionSensor } from "../entities/MotionSensor";
-import { Light } from "../entities/Light";
-import { Door } from "../entities/Door";
-import { SmokeAlarm } from "../entities/SmokeAlarm";
+import {
+  IEvent,
+  IObserver,
+  ISmokeAlarm,
+  IEventDispatcher,
+} from "../interfaces";
 
 export class FireSafety implements IObserver {
-  private sprinklers: Sprinkler[];
-  private occupancySensor: MotionSensor;
-  private doors: Door[];
-  private lights: Light[];
-
   constructor(
-    smokeAlarm: SmokeAlarm,
-    sprinklers: Sprinkler[],
-    occupancySensor: MotionSensor,
-    doors: Door[],
-    lights: Light[]
+    smokeAlarm: ISmokeAlarm,
+    private eventDispatcher: IEventDispatcher
   ) {
-    this.sprinklers = sprinklers;
-    this.occupancySensor = occupancySensor;
-    this.doors = doors;
-    this.lights = lights;
     smokeAlarm.subscribe(this);
   }
+
   update(event: IEvent): void {
     if (event.type === "SMOKE_DETECTED") {
-      if (this.occupancySensor.getOccupants() > 0) {
-        this.sprinklers.forEach((sprinkler) => {
-          sprinkler.setActive(true);
-        });
-      }
-
-      this.doors.forEach((door) => {
-        door.setLocked(false);
-      });
-
-      this.lights.forEach((light) => {
-        light.setOn(true);
-      });
+      this.eventDispatcher.dispatchEvent(event);
     }
   }
 }
