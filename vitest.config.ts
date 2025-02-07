da89c75f2049268a427c69399f04cf80172c2aba
Rafael Barros Parigi <rafael.barrosparigi@sky.uk>
2025-02-07 14:13:40 +0000

    add test coverage and change test folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // Tells Vitest to look for test files in the test directory
+    // Tells Vitest to look for test files in the src directory
     // that end with .test.ts or .spec.ts
-    include: ["test/**/*.{test,spec}.ts"],
+    include: ['src/**/*.{test,spec}.ts'],
 
     // Sets up the test environment
-    environment: "node",
+    environment: 'node',
 
     // Ensures tests are isolated from each other
     globals: true,
 
     // Adds coverage reporting
     coverage: {
-      provider: "v8",
-      reporter: ["text", "html"],
-      exclude: ["node_modules/"],
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/'],
     },
   },
 });
