9ff8109aaf01bcd403b6129b3ef6b8147eadfbd0
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-10 21:32:15 +0000

    added some linter rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,14 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  {
+    ignores: [
+      "coverage/**",
+      "src/index.ts",
+      "src/features/*",
+      "src/interfaces/index.ts",
+      "src/test/*",
+    ],
+  }
 );
