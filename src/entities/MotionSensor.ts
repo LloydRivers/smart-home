1c556a464018c8d63392c5002de4bbacd63e8a37
menaBM <mena.bousfieldmilton@sky.uk>
2025-02-10 17:01:42 +0000

    add setOccupants to MotionSensor class

--- src/entities/MotionSensor.ts
+++ src/entities/MotionSensor.ts
@@ -1,6 +1,10 @@
 export class MotionSensor {
   private occupants: number = 0;
 
+  setOccupants(ocupants: number): void {
+    this.occupants = ocupants
+  }
+
   getOccupants(): number {
     return this.occupants;
   }
