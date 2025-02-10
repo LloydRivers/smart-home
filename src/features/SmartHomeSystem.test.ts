38ff2bea34c41d3dfd37174f1314edae8a0b54bf
Rafael Barros Parigi <rafael.barrosparigi@sky.uk>
2025-02-10 11:51:57 +0000

    add tests for SmartHomeSystem

--- src/features/SmartHomeSystem.test.ts
+++ src/features/SmartHomeSystem.test.ts
@@ -1,75 +1,77 @@
+import { vi } from 'vitest';
 import { ConsoleLogger } from '../utils/Logger';
-import { SmartHomeSystem } from './SmartHomeSystem';
+import { SmartHomeSystem, TemperatureControl } from './SmartHomeSystem';
 
 const logger = new ConsoleLogger();
+const temperatureControl: TemperatureControl = {
+  _isActive: false,
+  _temperature: 29,
+};
+const smartHomeSystem = new SmartHomeSystem(logger, temperatureControl);
+const mockedDate = new Date('2024-02-09T12:00:00Z');
+
+const notifySpy = vi.spyOn(smartHomeSystem, 'notify');
+
+beforeEach(() => {
+  vi.setSystemTime(mockedDate);
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+  vi.clearAllMocks();
+});
 
 describe('Feature: EnergyEfficiency', () => {
-  it.only('SHOULD create a new EnergyControl instance correctly', () => {
-    const energyControl = new SmartHomeSystem(logger, []);
-    expect(energyControl).toBeInstanceOf(SmartHomeSystem);
-    expect(energyControl).toMatchInlineSnapshot(`
-        EnergyControl {
-          "_observers": Set {},
-          "logger": ConsoleLogger {},
-          "thermostats": [],
-        }
+  it('SHOULD create a new EnergyControl instance correctly', () => {
+    expect(smartHomeSystem).toBeInstanceOf(SmartHomeSystem);
+    expect(smartHomeSystem).toMatchInlineSnapshot(`
+      SmartHomeSystem {
+        "_temperatureControl": {
+          "_isActive": false,
+          "_temperature": 29,
+        },
+        "logger": ConsoleLogger {},
+        "observers": Set {},
+      }
     `);
   });
 });
 
-//   describe('Methods: ', () => {
-//     describe('GETTERS: ', () => {
-//       it.only('temperature: SHOULD return the correct temperature', () => {
-//         const energyControl = new EnergyControl(logger, []);
-//         const newThermostat = new Thermostat({
-//           energyControl,
-//           initialTemperature: 25,
-//         });
+describe('Methods: ', () => {
+  describe('GETTERS: ', () => {
+    it('temperatureControl: SHOULD return the correct temperature control values', () => {
+      expect(smartHomeSystem.temperatureControl).toEqual(temperatureControl);
+    });
+  });
 
-//         energyControl.subscribe(newThermostat);
+  describe('SETTERS: ', () => {
+    describe('temperature: ', () => {
+      it('SHOULD set the new temperature correctly WHEN it is within acceptable range', () => {
+        smartHomeSystem.temperature = 10;
 
-//         expect(energyControl.observers).toEqual(new Set([newThermostat]));
-//       });
+        expect(smartHomeSystem.temperatureControl._temperature).toEqual(10);
+        expect(notifySpy).toHaveBeenCalledWith({
+          type: 'TEMPERATURE_CHANGE',
+          timestamp: mockedDate,
+          payload: smartHomeSystem.temperatureControl._temperature,
+        });
+      });
 
-//       it('isActive: SHOULD return the correct activation state', () => {
-//         const thermostat = new Thermostat({
-//           energyControl,
-//           initialTemperature: 30,
-//         });
-//         expect(thermostat.isActive).toEqual(false);
-//       });
-//     });
+      it('SHOULD NOT set the new temperature correctly WHEN it is NOT within acceptable range', () => {
+        expect(() => {
+          smartHomeSystem.temperature = 5;
+        }).toThrow('Temperature is out of valid range');
+      });
+    });
 
-//     describe('SETTERS: ', () => {
-//       it('temperature: SHOULD set the temperature correctly', () => {
-//         const thermostat = new Thermostat({
-//           energyControl,
-//           initialTemperature: 30,
-//         });
-//         thermostat.temperature = 19;
-//         expect(thermostat.temperature).toEqual(19);
-//       });
-//     });
-
-//     it('activate: SHOULD set the isActive state to true', () => {
-//       const thermostat = new Thermostat({
-//         energyControl,
-//         initialTemperature: 30,
-//       });
-//       thermostat.activate();
-//       expect(thermostat.isActive).toEqual(true);
-//     });
-
-//     it('deactivate: SHOULD set the isActive state to true', () => {
-//       const thermostat = new Thermostat({
-//         energyControl,
-//         initialTemperature: 30,
-//       });
-//       thermostat.activate(); // make sure the thermostat is active before deactivating it
-//       thermostat.deactivate();
-//       expect(thermostat.isActive).toEqual(false);
-//     });
-
-//     it('test implementations/updates');
-//   });
-// });
+    it('isActive: SHOULD set the isActive state correctly', () => {
+      smartHomeSystem.isActive = true;
+      expect(smartHomeSystem.temperatureControl._isActive).toEqual(true);
+      expect(notifySpy).toHaveBeenCalledWith({
+        type: 'ISACTIVE_CHANGE',
+        timestamp: mockedDate,
+        payload: smartHomeSystem.temperatureControl._isActive,
+      });
+    });
+  });
+});
